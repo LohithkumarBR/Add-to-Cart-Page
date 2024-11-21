17b4fb87b943fa972fc847407ea7a4da2689acaf
Lohith kumar B R <169041735+LohithkumarBR@users.noreply.github.com>
2024-11-21 21:38:36 +0530

    Add files via upload

---
+++ Add_to_Cart_Page.js
@@ -0,0 +1,15 @@
+let cartItemTextInputE1 = document.getElementById("cartItemTextInput");
+let cartItemsContainerE1 = document.getElementById("cartItemsContainer");
+
+function onAddCartItem() {
+    let cartItemText = cartItemTextInputE1.value;
+
+    if (cartItemText === "") {
+        alert("Enter valid Item")
+    }
+    let cartItemE1 = document.createElement("li");
+    cartItemE1.textContent = cartItemText;
+
+    cartItemTextInputE1.value = "";
+    cartItemsContainerE1.appendChild(cartItemE1);
+}
